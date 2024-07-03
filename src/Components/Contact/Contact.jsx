78b2d11a60cd5a617dcfa0ecb58cdbb717e6e283
Samolyb <artemte6575@gmail.com>
2024-07-03 20:20:53 +0100

    React-hm-3

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,22 @@
+import css from "./Contact.module.css";
+
+export const Contact = ({ user: { id, name, number }, onDelete }) => {
+    return (
+        <div className={css.maincontainer}>
+            <div className={css.container}>
+                <p>
+                    <FaUser className={css.icon} />
+                    {name}
+                </p>
+                <p>
+                    <FaPhone className={css.icon} />
+                    {number}
+                </p>
+            </div>
+            <button className={css.btn} onClick={() => onDelete(id)}>
+                <FaUserMinus className={css.icon} />
+                Delete
+            </button>
+        </div>
+    );
+};
